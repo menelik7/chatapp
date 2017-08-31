8a565cfd03cd0b834d61b27aa3472dcb599e55de
kamoltip <plawansusu@gmail.com>
2017-08-31 00:37:24 -0400

    init

--- app/models/message.js
+++ app/models/message.js
@@ -1,44 +1,43 @@
-// Dependencies
-// =============================================================
+
 
 // This may be confusing but here Sequelize (capital) references the standard library
-// var Sequelize = require("sequelize");
+var Sequelize = require("sequelize");
 // sequelize (lowercase) references our connection to the DB.
-// var sequelize = require("../config/connection.js");
+var sequelize = require("../config/connection.js");
 
-// // Creates a "Message" model that matches up with DB
-// var Message = sequelize.define("message", {
-//   author: {
-//     type: Sequelize.STRING
-//   },
-//   body: {
-//     type: Sequelize.STRING
-//   },
-//   created_at: {
-//     type: Sequelize.DATE
-//   }
-// }, {
-//   timestamps: false
-// });
+// Creates a "Message" model that matches up with DB
+var Message = sequelize.define("message", {
+  author: {
+    type: Sequelize.STRING
+  },
+  body: {
+    type: Sequelize.STRING
+  },
+  created_at: {
+    type: Sequelize.DATE
+  }
+}, {
+  timestamps: false
+});
 
-// // Syncs with DB
-// Message.sync();
+// Syncs with DB
+Message.sync();
 
-// // Makes the Message Model available for other files (will also create a table)
-// module.exports = Message;
+// Makes the Message Model available for other files (will also create a table)
+module.exports = Message;
 
-module.exports = function(sequelize, DataTypes) {
-  // Define the Message sequelize model
-  var Message = sequelize.define("message", {
-    author: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    body: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  });
+// module.exports = function(sequelize, DataTypes) {
+//   // Define the Message sequelize model
+//   var Message = sequelize.define("message", {
+//     author: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     body: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     }
+//   });
 
-  return Message;
-};
+//   return Message;
+// };
